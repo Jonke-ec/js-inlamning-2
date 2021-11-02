0093c7007b84183bd35beee68d758c2997a4e3d9
Jonke-ec <jonas.askling@utb.ecutbildning.se>
2021-11-02 15:40:25 +0100

    app

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -4,9 +4,9 @@ const MyCard = () => {
     return (
         <div className="card w-50 container m-3">
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title">Juni Ida Linéa Askling</h5>
+                <h6 className="card-subtitle mb-2 text-muted">juni.askling@gmail.com</h6>
+                <p className="card-text">Världens bästa bebis.</p>
             </div>
         </div>
     )
